687cd09f3d6b7443297a96eca49c909f96957c3e
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-08-21 22:14:50 -0400

    [ADD] Finalizado Votacao

--- src/views/Afternoon.vue
+++ src/views/Afternoon.vue
@@ -1,78 +1,111 @@
 <template>
   <div class="afternoon bg-pan-left">
     <div class="box">
-      <One v-on:click.native="enviarVoto('Sir-Barman')"/>
-      <Two v-on:click.native="enviarVoto('TecSoc')"/>
-      <three v-on:click.native="enviarVoto('CDF-Transportes')"/>
-      <Four v-on:click.native="enviarVoto('NewFast')"/>
+      <One v-on:click.native="enviarVoto('Sir-Barman')" />
+      <Two v-on:click.native="enviarVoto('TecSoc')" />
+      <three v-on:click.native="enviarVoto('CDF-Transportes')" />
+      <Four v-on:click.native="enviarVoto('NewFast')" />
     </div>
   </div>
 </template>
 
 <script>
-  import firebase from 'firebase';
-  import One from '@/components/Afternoon/One.vue';
-  import Two from '@/components/Afternoon/Two.vue';
-  import Three from '@/components/Afternoon/Three.vue';
-  import Four from '@/components/Afternoon/Four.vue';
+import firebase from "firebase";
+import One from "@/components/Afternoon/One.vue";
+import Two from "@/components/Afternoon/Two.vue";
+import Three from "@/components/Afternoon/Three.vue";
+import Four from "@/components/Afternoon/Four.vue";
 
-  export default {
-    name: 'Afternoon',
-    components: {
-      One,
-      Two,
-      Three,
-      Four
-    },
-    data(){
-      return{
-        jaVotou: false
-      }
-    },
-    methods:{
-      async enviarVoto(opcao){
-
-        if(jaVotou){
-          // Corno já votou
-          return;
+export default {
+  name: "Afternoon",
+  components: {
+    One,
+    Two,
+    Three,
+    Four
+  },
+  data() {
+    return {
+      jaVotou: false,
+      equipes: {
+        one: {
+          nome: "Sir-Barman",
+          pontos: 0
+        },
+        two: {
+          nome: "TecSoc",
+          pontos: 0
+        },
+        three: {
+          nome: "CDF-Transportes",
+          pontos: 0
+        },
+        four: {
+          nome: "NewFast",
+          pontos: 0
         }
-        
-        let valorAntigo = await firebase.database()
+      }
+    };
+  },
+  methods: {
+    async enviarVoto(opcao) {
+
+      if (this.jaVotou) {
+        // Corno já votou
+        return;
+      }
+
+      let valorAntigo = (await firebase
+        .database()
+        .ref(opcao)
+        .once("value")).val();
+
+      console.log(valorAntigo);
+
+      valorAntigo = valorAntigo ? valorAntigo : 0;
+
+      try {
+        await firebase
+          .database()
           .ref(opcao)
-          .once('value');
-        
-        valorAntigo = valorAntigo ? valorAntigo : 0;
+          .set(++valorAntigo);
+        // Sucesso
 
-        try{
-          await firebase.database()
-            .ref(opcao)
-            .set(++valorAntigo);
-          // Sucesso
-
-          jaVotou = true;
-        }catch(e){
-          // Fudeo de vez 
-
-        }
+        pesquisarDados()
+        this.jaVotou = true;
+      } catch (e) {
+        // Fudeo de vez
       }
+    },
+    async pesquisarDados() {
+      equipes.array.forEach(element => {
+        firebase
+          .database()
+          .ref(element.nome)
+          .then(e => {
+            e = e.val();
+            element.pontos = e ? e : 0
+          });
+      });
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
-  .afternoon {
-    height: 100%;
-    background-image: url("../assets/svgs/one.svg");
-    background-size: cover;
-    background-position: center right;
-  }
+.afternoon {
+  height: 100%;
+  background-image: url("../assets/svgs/one.svg");
+  background-size: cover;
+  background-position: center right;
+}
 
-  .box {
-    margin-left: 50%;
-    width: 50%;
-    min-height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-items: flex-end;
-  }
+.box {
+  margin-left: 50%;
+  width: 50%;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-items: flex-end;
+}
 </style>
