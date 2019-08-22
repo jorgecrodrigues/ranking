3aa30bcc2b88f7718b2d2cbdd04eff8ccee3c489
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-08-21 22:40:11 -0400

    FIX

--- src/views/Afternoon.vue
+++ src/views/Afternoon.vue
@@ -2,15 +2,15 @@
   <div class="afternoon bg-pan-left">
     <div class="box">
       <div class="box-left">
-        <h1 class="question">
-          Qual destes negócios você acredita que tem mais potencial para ser lançado no mercado?
-        </h1>
+        <h1
+          class="question"
+        >Qual destes negócios você acredita que tem mais potencial para ser lançado no mercado?</h1>
       </div>
       <div class="box-right">
-        <One v-on:click.native="enviarVoto('Sir-Barman')"/>
-        <Two v-on:click.native="enviarVoto('TecSoc')"/>
-        <three v-on:click.native="enviarVoto('CDF-Transportes')"/>
-        <Four v-on:click.native="enviarVoto('NewFast')"/>
+        <One v-on:click.native="enviarVoto('Sir-Barman')" />
+        <Two v-on:click.native="enviarVoto('TecSoc')" />
+        <three v-on:click.native="enviarVoto('CDF-Transportes')" />
+        <Four v-on:click.native="enviarVoto('NewFast')" />
       </div>
     </div>
   </div>
@@ -56,8 +56,7 @@ export default {
   },
   methods: {
     async enviarVoto(opcao) {
-
-      if(jaVotou){
+      if (this.jaVotou) {
         // Corno já votou
         return;
       }
@@ -70,71 +69,105 @@ export default {
       valorAntigo = valorAntigo ? valorAntigo : 0;
 
       try {
+        this.jaVotou = true;
         await firebase
           .database()
           .ref(opcao)
           .set(++valorAntigo);
         // Sucesso
 
-        pesquisarDados()
-        this.jaVotou = true;
+        pesquisarDados();
       } catch (e) {
         // Fudeo de vez
       }
     },
     async pesquisarDados() {
-      equipes.array.forEach(element => {
-        firebase
-          .database()
-          .ref(element.nome)
-          .then(e => {
-            e = e.val();
-            element.pontos = e ? e : 0
-          });
-      });
+      let one = this.equipes.one;
+
+      firebase
+        .database()
+        .ref(one.nome)
+        .once("value")
+        .then(e => {
+          e = e.val();
+          one.pontos = e ? e : 0;
+        });
+
+      let two = this.equipes.two;
+
+      firebase
+        .database()
+        .ref(two.nome)
+        .once("value")
+        .then(e => {
+          e = e.val();
+          two.pontos = e ? e : 0;
+        });
+
+      let three = this.equipes.three;
+
+      firebase
+        .database()
+        .ref(three.nome)
+        .once("value")
+        .then(e => {
+          e = e.val();
+          three.pontos = e ? e : 0;
+        });
+
+      let four = this.equipes.four;
+
+      firebase
+        .database()
+        .ref(four.nome)
+        .once("value")
+        .then(e => {
+          e = e.val();
+          four.pontos = e ? e : 0;
+        });
     }
   },
-  mounted(){
+  mounted() {
     this.pesquisarDados();
   }
 };
 </script>
 
 <style scoped>
-  .afternoon {
-    height: 100%;
-  }
+.afternoon {
+  height: 100%;
+}
 
-  .question {
-    width: 60%;
-    padding: 5rem;
-    background: rgba(255, 255, 255, 0.95);
-    border-radius: 10px;
-    font-size: 3rem;
-    color: black;
-  }
+.question {
+  width: 60%;
+  padding: 5rem;
+  background: rgba(255, 255, 255, 0.95);
+  border-radius: 10px;
+  font-size: 3rem;
+  color: black;
+}
 
-  .box {
-    min-height: 100%;
-    display: flex;
-  }
+.box {
+  min-height: 100%;
+  display: flex;
+}
 
-  .box-left,
-  .box-right {
-    width: 50%;
-  }
+.box-left,
+.box-right {
+  width: 50%;
+}
 
-  .box-left {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.box-left {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .box-right {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    flex-direction: column;
-  }
+.box-right {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
 </style>
