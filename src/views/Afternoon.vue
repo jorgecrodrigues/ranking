ae214a07d928a2e663341678cabc474088f47b8c
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-08-21 22:40:50 -0400

    Alterado código css

--- src/views/Afternoon.vue
+++ src/views/Afternoon.vue
@@ -17,84 +17,84 @@
 </template>
 
 <script>
-import firebase from "firebase";
-import One from "@/components/Afternoon/One.vue";
-import Two from "@/components/Afternoon/Two.vue";
-import Three from "@/components/Afternoon/Three.vue";
-import Four from "@/components/Afternoon/Four.vue";
+  import firebase from 'firebase';
+  import One from '@/components/Afternoon/One.vue';
+  import Two from '@/components/Afternoon/Two.vue';
+  import Three from '@/components/Afternoon/Three.vue';
+  import Four from '@/components/Afternoon/Four.vue';
 
-export default {
-  name: "Afternoon",
-  components: {
-    One,
-    Two,
-    Three,
-    Four
-  },
-  data() {
-    return {
-      jaVotou: false,
-      equipes: {
-        one: {
-          nome: "Sir-Barman",
-          pontos: 0
-        },
-        two: {
-          nome: "TecSoc",
-          pontos: 0
-        },
-        three: {
-          nome: "CDF-Transportes",
-          pontos: 0
-        },
-        four: {
-          nome: "NewFast",
-          pontos: 0
+  export default {
+    name: 'Afternoon',
+    components: {
+      One,
+      Two,
+      Three,
+      Four
+    },
+    data() {
+      return {
+        jaVotou: false,
+        equipes: {
+          one: {
+            nome: 'Sir-Barman',
+            pontos: 0
+          },
+          two: {
+            nome: 'TecSoc',
+            pontos: 0
+          },
+          three: {
+            nome: 'CDF-Transportes',
+            pontos: 0
+          },
+          four: {
+            nome: 'NewFast',
+            pontos: 0
+          }
         }
-      }
-    };
-  },
-  methods: {
-    async enviarVoto(opcao) {
+      };
+    },
+    methods: {
+      async enviarVoto(opcao) {
 
-      if(jaVotou){
-        // Corno já votou
-        return;
-      }
+        if (jaVotou) {
+          // Corno já votou
+          return;
+        }
 
-      let valorAntigo = (await firebase
-        .database()
-        .ref(opcao)
-        .once("value")).val();
-
-      valorAntigo = valorAntigo ? valorAntigo : 0;
-
-      try {
-        await firebase
+        let valorAntigo = (await firebase
           .database()
           .ref(opcao)
-          .set(++valorAntigo);
-        // Sucesso
+          .once('value')).val();
 
-        pesquisarDados()
-        this.jaVotou = true;
-      } catch (e) {
-        // Fudeo de vez
+        valorAntigo = valorAntigo ? valorAntigo : 0;
+
+        try {
+          await firebase
+            .database()
+            .ref(opcao)
+            .set(++valorAntigo);
+          // Sucesso
+
+          pesquisarDados();
+          this.jaVotou = true;
+        } catch (e) {
+          // Fudeo de vez
+        }
+      },
+      async pesquisarDados() {
+        equipes.array.forEach(element => {
+          firebase
+            .database()
+            .ref(element.nome)
+            .then(e => {
+              e = e.val();
+              element.pontos = e ? e : 0;
+            });
+        });
       }
-    },
-    async pesquisarDados() {
-      equipes.array.forEach(element => {
-        firebase
-          .database()
-          .ref(element.nome)
-          .then(e => {
-            e = e.val();
-            element.pontos = e ? e : 0
-          });
-      });
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
